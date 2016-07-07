de197c158c30694fb1e722a519cb00f8fb3dc1e9
John Oerter <joerter@gmail.com>
2016-07-07 08:33:43 -0500

    Update hostname

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rsync ./docker-compose.production.yml chat@chat.johnoerter.me:/home/chat
+rsync ./docker-compose.production.yml chat@gifroulette.johnoerter.me:/home/chat
 ssh chat@chat.johnoerter.me docker-compose -f docker-compose.production.yml stop
 ssh chat@chat.johnoerter.me docker pull joerter/gifroulette:latest
 ssh chat@chat.johnoerter.me docker-compose -f docker-compose.production.yml up -d
